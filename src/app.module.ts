f31216b9e2fdf77a54a98272af0543036e536d8c
MrAnton777 <anton20ivanoff@yandex.ru>
2025-02-15 11:36:32 +0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './books/book.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [BookModule],
+  imports: [BookModule,TypeOrmModule.forRoot({
+    type:'mysql',
+    host:'localhost',
+    port:3306,
+    username:"root",
+    password:"root",
+    database:"test",
+    entities:[],
+    synchronize:true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
